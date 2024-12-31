3e3c92001c01a61b0d05606eb3ebb0b710652203
shao0228 <3076698874@qq.com>
2024-12-31 11:14:52 +0800

    update

---
+++ 12.30.2/script.js
@@ -0,0 +1,4 @@
+let mountains = ['Everest', 'Fuji', 'Nanga Parbat'];
+mountains[2] = 'K2';
+
+console.log(mountains);
---
+++ 12.31.1/remainder.js
@@ -0,0 +1,4 @@
+let f1 = 'apple',
+    f2 = 'Banana';
+let result = f2.toLowerCase() < f1.toLowerCase();
+console.log(result);
